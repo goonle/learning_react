91ab132ee9f82475a07e8670f82f8db6e140adc3
goonle <junhuh741@gmail.com>
2025-02-06 22:40:20 +1300

    axios and middleware

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY='9f17007d27099b0f6005027f06a0fa78';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Action creator always return action
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},nz`;
+    const request = axios.get(url);
+
+    // And Action is always object. 
+    return {
+        // And Action has always type
+        type: FETCH_WEATHER,
+        payload: request 
+    };
+}
