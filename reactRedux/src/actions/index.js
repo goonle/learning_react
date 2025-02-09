59bb313d328e625cd654987619c25f6742272df2
goonle <junhuh741@gmail.com>
2025-02-09 18:51:50 +1300

    create credentialKey.js to hide api key

--- reactRedux/src/actions/index.js
+++ reactRedux/src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import API_KEY from './credentialKey';
 
-const API_KEY='9f17007d27099b0f6005027f06a0fa78';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 // Action creator always return action
